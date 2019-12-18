22e38ec886640d70b6300e0e7ce38a2f68f76132
Jeongho Noh <nossi8128@gmail.com>
2019-12-18 14:38:37 +0900

    wherever you press 'Enter' key, submite would be excuted

--- src/pages/MyGit/index.js
+++ src/pages/MyGit/index.js
@@ -8,34 +8,42 @@ const initGit = {
 function MyGit() {
   const [gitInfo, setGitInfo] = useState(initGit);
   const [userID, setUserID] = useState('');
-  const getUserIdHandle = e => {
+  const getUserIdHandler = e => {
     setUserID(e.target.value);
   };
-  const fetchHandle = async () => {
+  const fetchHandler = async () => {
     const data = await fetch(`https://api.github.com/users/${userID}`);
     const { login, public_repos, followers } = await data.json();
     const newGitInFo = { login, public_repos, followers };
     console.log(newGitInFo);
     setGitInfo(newGitInFo);
   };
-  const keyPressHandle = e => {
-    if (e.key === 'Enter') fetchHandle();
+  const keyPressHandler = e => {
+    if (e.key === 'Enter') fetchHandler();
   };
+
+  useEffect(() => {
+    window.addEventListener('keypress', keyPressHandler);
+    return () => {
+      window.removeEventListener('keypress', keyPressHandler);
+    };
+  });
+
   return (
     <>
       <h1>useEffect, Fetch</h1>
       <h2>userID: {gitInfo.login}</h2>{' '}
       <h2>public_repos: {gitInfo.public_repos}</h2>
       <h2> followers: {gitInfo.followers}</h2>
-      <span>write git uer ID</span>
+      <span>Write Git User ID</span>
       <input
         type="text"
         value={userID}
         name="nono"
-        onChange={getUserIdHandle}
-        onKeyPress={keyPressHandle}
+        onChange={getUserIdHandler}
+        //onKeyPress={keyPressHandler}
       ></input>
-      <button className="fetch-button" onClick={fetchHandle}>
+      <button className="fetch-button" onClick={fetchHandler}>
         fetch
       </button>
     </>
